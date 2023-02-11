30db31da0d334fad7cdff63d924c39a7c9c02d5f
Devendra Milmile <d.h.milmile1@gmail.com>
2023-02-12 03:41:31 +0530

    Update scroll bar css and remvoed default html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, StoreModule.forRoot({}, {})],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		StoreModule.forRoot({}, {})
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
